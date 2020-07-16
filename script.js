69405be0e451fce4f4a1011e56b5810c40cb2d71
Will Rodgers <WillRodgers.e.s@gmail.com>
2020-07-16 15:20:29 -0700

    update

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+function generatePassword() {
+	var ask = prompt(
+		"Please choose a length of at least 8 characters and no more than 128 characters"
+	);
+	if (person != null) {
+		document.getElementById("demo").innerHTML =
+			"Hello " + person + "! How are you today?";
+	}
+	////
+	///
+	return "this is a test";
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+	var password = generatePassword();
+	console.log(password);
+	var passwordText = document.querySelector("#password");
+	passwordText.value = password;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
